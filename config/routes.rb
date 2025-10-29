e87317845aa503b3564fed1705efb7d864333057
Candido Gomes <candidosg@gmail.com>
2025-10-29 14:47:07 -0400

    feat: add Mission Control Jobs integration and configure HTTP Basic Authentication in development

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   if Rails.env.development?
     mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
   end
+
+  mount MissionControl::Jobs::Engine, at: "/jobs"
+
   post "/graphql", to: "graphql#execute"
   root "articles#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
